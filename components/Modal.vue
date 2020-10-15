e17666dc4f9e10c3000ed528ce38400a16800e21
JustZoB <37073994+JustZoB@users.noreply.github.com>
2020-10-15 15:50:01 +0600

    Delete basic files, add modal and table components, store

---
+++ components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="text-center">
+        <v-dialog v-model="dialog" width="500">
+            <v-card>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                        <v-btn
+                            color="primary"
+                            text
+                            @click="dialog = false"
+                        >
+                            Close
+                        </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            dialog: false,
+        }
+    },
+}
+</script>
---
+++ components/Table.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="users"
+        :items-per-page="10"
+        class="elevation-1"
+    ></v-data-table>
+</template>
+
+<script>
+export default {
+    mounted() {
+        this.getUsers()
+    },
+    data () {
+        return {
+            headers: [
+                { text: "User ID", value: "userId" },
+                { text: "ID", value: "id" },
+                { text: "Title", value: "title" },
+                { text: "Body", value: "body" }
+            ],
+            users: []
+        }
+    },
+    methods: { 
+        async getUsers() {
+            let res = await this.$store.dispatch('GET_USERS')
+            this.users = res.data
+        },
+    },
+}
+</script>
