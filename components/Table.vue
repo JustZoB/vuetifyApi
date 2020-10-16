fb3bc637f3d1490fe293a4b25a2691d343c66e24
JustZoB <37073994+JustZoB@users.noreply.github.com>
2020-10-16 15:16:35 +0600

    Delete store and modal component. Add modal in table component, add CRUD actions in table

--- components/Table.vue
+++ components/Table.vue
@@ -1,33 +1,71 @@
 <template>
-    <v-data-table
-        :headers="headers"
-        :items="users"
-        :items-per-page="10"
-        class="elevation-1"
-    ></v-data-table>
+    <v-card>
+        <v-data-table
+            :headers="headers"
+            :items="users"
+            :items-per-page="10"
+            class="elevation-1"
+            @click:row="editItem"
+        >
+        </v-data-table>
+        <v-dialog v-model="dialog" max-width="500px">
+            <v-card>
+                <v-card-actions>
+                    <v-col cols="12">
+                        <v-form ref="form">
+                            <v-text-field label="ID" v-model="editedItem.id" readonly></v-text-field>
+                            <v-text-field label="User ID" v-model="editedItem.userId"></v-text-field>
+                            <v-text-field label="Title" v-model="editedItem.title"></v-text-field>
+                            <v-text-field label="Body" v-model="editedItem.body"></v-text-field>
+                            <v-btn color="primary" text @click="deleteItem">Delete</v-btn>
+                            <v-btn color="primary" text @click="closeDialog">Close</v-btn>
+                        </v-form>
+                    </v-col>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-card>
 </template>
 
 <script>
 export default {
-    mounted() {
-        this.getUsers()
-    },
     data () {
         return {
+            dialog: false,
             headers: [
                 { text: "User ID", value: "userId" },
                 { text: "ID", value: "id" },
                 { text: "Title", value: "title" },
                 { text: "Body", value: "body" }
             ],
-            users: []
+            users: [],
+            editedItem: {
+                id: 0,
+                userId: 0,
+                title: "",
+                body: "",
+            }
         }
     },
+    created() {
+        this.initialize()
+    },
     methods: { 
-        async getUsers() {
-            let res = await this.$store.dispatch('GET_USERS')
+        async initialize() {
+            let res = await this.$axios.get('http://jsonplaceholder.typicode.com/posts')
             this.users = res.data
         },
+        editItem(item) {
+            this.editedItem = item
+            this.dialog = true
+        },
+        deleteItem() {
+            this.users.splice(this.users.indexOf(this.editedItem), 1)
+            this.closeDialog()
+        },
+        closeDialog() {
+            this.dialog = false
+        }
     },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,14 @@
 <template>
     <div>
         <Table />
-        <Modal />
     </div>
 </template>
 
 <script>
-import Modal from "@/components/Modal.vue"
 import Table from "@/components/Table.vue"
 
 export default {
     components: {
-        Modal,
         Table
     },
 }
